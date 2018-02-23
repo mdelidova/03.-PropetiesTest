9a4152ba82d50a87596decbf51cc93b892db9572
Maryna Delidova <marynadelidova@marynas-macbook-pro.local>
2018-02-23 18:18:38 +0200

    Initial Commit

---
+++ PropetiesTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PropetiesTest
+//
+//  Created by Maryna Delidova on 2/23/18.
+//  Copyright © 2018 Maryna Delidova. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
